864e53dfb943245daa3f6caa3870158b27256ca6
VincentC27 <18120867+CVincent27@users.noreply.github.com>
2025-03-25 14:22:29 +0100

    add extracted_data et def fonctions get_data

--- src/01_extract.py
+++ src/01_extract.py
@@ -1,18 +1,44 @@
 from config import init_spark, load_spark
 import requests
 
-try:
-    spark, sc = load_spark()
-except FileNotFoundError:
-    print("Initialisation d'une nouvelle session Spark...")
-    spark, sc = init_spark()
+def init_or_load_spark():
+    try:
+        spark, sc = load_spark()
+    except FileNotFoundError:
+        print("Init nouvelle session Spark")
+        spark, sc = init_spark()
+    return spark
 
-url = "https://data.lillemetropole.fr/data/ogcapi/collections/ilevia:vlille_temps_reel/items?f=json&limit=-1"
+def get_data_vlille(spark):
+    url = "https://data.lillemetropole.fr/data/ogcapi/collections/ilevia:vlille_temps_reel/items?f=json&limit=-1"
+    
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        data = response.json()
+        print("données V'lille DL")
+    else:
+        print(f"erreur: {response.status_code}")
+        return  
 
-response = requests.get(url)
+    print(type(data))
+    records = data.get("records", [])
+    print(type(records))
+    print(records[:1])
 
-if response.status_code == 200:
-    data = response.json()
-    print("données Vlille DL")
-else:
-    print(f"erreur: {response.status_code}")
+    extracted_data = [{
+        'id': record.get('@id', None),
+        'nom': record.get('nom', None),
+        'etat' : record.get('etat', None),
+        'nb_places_dispo' : record.get('nb_places_dispo', None),
+        'nb_velos_dispo' : record.get('nb_velos_dispo', None),
+        'etat_connexion' : record.get('etat_connexion', None),
+        'x' : record.get('x', None),
+        'y' : record.get('y', None),
+        'date' : record.get('date_modification', None),
+    } for record in records]    
+    df_spark = spark.createDataFrame(extracted_data)
+    df_spark.show(5)
+
+spark = init_or_load_spark()
+get_data_vlille(spark)
