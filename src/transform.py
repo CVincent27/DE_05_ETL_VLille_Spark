533f88bd35af0c52923508795a6a1ea4a535e777
VincentC27 <18120867+CVincent27@users.noreply.github.com>
2025-03-26 12:02:26 +0100

    test transfo + update import

--- src/transform.py
+++ src/transform.py
@@ -1,8 +1,13 @@
-from extract import init_or_load_spark, get_data_vlille
+from extract import init_or_load_spark, get_extracted_data, get_data_vlille
+from pyspark.sql import functions as F
 
 spark = init_or_load_spark()
-df_spark = get_data_vlille(spark)
+records = get_data_vlille(spark)
+df_spark = get_extracted_data(spark, records)
 
-df_spark.show(1)
+# df_spark.show(1)
 
-
+# Transformation format date
+df_spark = df_spark.withColumn('date', F.date_format(F.to_timestamp(df_spark['date'], 'yyyy-MM-dd\'T\'HH:mm:ss.SSSXXX'), 'yyyy-MM-dd HH:mm'))
+df_spark = df_spark.orderBy(F.desc('date'))
+df_spark.show(2)
