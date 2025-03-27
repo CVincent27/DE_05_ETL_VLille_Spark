59a3f65c5d3a7ec25962844f4a904344ac15346d
VincentC27 <18120867+CVincent27@users.noreply.github.com>
2025-03-27 14:54:27 +0100

    add function transfo id + format date

--- src/transform.py
+++ src/transform.py
@@ -23,13 +23,12 @@ def clean_data(df_raw_data):
     if df_raw_data is None:
         print("Dataframe introuvable")
         return None
-
     ## Liste valeurs de etat et etat connexion
     df_raw_data.select('etat', 'etat_connexion').distinct().show()
     ## add df reformed_data et filtre sur le df raw_data
     df_reformed_data = df_raw_data.filter(col("etat") == "RÉFORMÉ")
     df_data_filtre = df_raw_data.filter(col("etat") != "RÉFORMÉ")
-    print(f"Nombre de stations réformée: {df_reformed_data.count()}")
+    print(f"Nombre de stations réformées: {df_reformed_data.count()}")
     # df_reformed_data.show(2)
     df_data_filtre.select('etat', 'etat_connexion').distinct().show()
     ## Export reformed vers json
@@ -49,12 +48,26 @@ def format_date(df_data_filtre):
 
 # Ajout de col nb_places_total et %_full
 def add_col_infos(df_format_date):
-    df_data_add_col = df_format_date.withColumn('nb_places_totales', df_format_date['nb_places_dispo'] + df_format_date['nb_velos_dispo'])
-    df_data_add_col = df_data_add_col.withColumn('%_full',((col('nb_velos_dispo')) / col('nb_places_totales')) * 100)
-    df_data_add_col = df_data_add_col.withColumn('%_full', round(col('%_full'), 2))
-    df_data_add_col.show(3)
-    print(df_data_add_col.filter(col('%_full').isNull()).count())
+    df_data_add_col = (df_format_date
+                       .withColumn('nb_places_totales', df_format_date['nb_places_dispo'] + df_format_date['nb_velos_dispo'])
+                       .withColumn('%_full', ((col('nb_velos_dispo')) / col('nb_places_totales')) * 100)
+                       .withColumn('%_full', round(col('%_full'), 2))
+    )
+    # df_data_add_col.show(3)
     return df_data_add_col
+
+def transformation_id(df_data_add_col):
+    # vlille_temps_reel.1 -> 1
+    df_clean_id = df_data_add_col.withColumn(
+    'id',
+    (F.regexp_extract(df_data_add_col['id'], r'\.(\d+)', 1))
+    # r'\.(\d+) cherche un . suivi d'un chiffre (\d+) et extrait le chiffre
+    )
+    df_clean_id.show(5)
+    # check unique id
+    check_id = df_clean_id.groupBy('id').count().filter(F.col('count') > 1)
+    check_id.show()
+    return df_clean_id
 
 if __name__ == "__main__":
     spark = init_or_load_spark()
@@ -62,4 +75,5 @@ if __name__ == "__main__":
     if df_raw_data:
         df_data_filtre = clean_data(df_raw_data)
         df_format_date = format_date(df_data_filtre)
-        add_col_infos(df_format_date)
+        df_data_add_col = add_col_infos(df_format_date)
+        transformation_id(df_data_add_col)
