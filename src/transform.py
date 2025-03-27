0a66df617283c3e21b1a548376ab42bca92b108d
VincentC27 <18120867+CVincent27@users.noreply.github.com>
2025-03-27 14:06:00 +0100

    add check_duplicates fontion

--- src/transform.py
+++ src/transform.py
@@ -23,28 +23,23 @@ def clean_data(df_raw_data):
     if df_raw_data is None:
         print("Dataframe introuvable")
         return None
-    
-    # 1. Data cleaning
+
     ## Liste valeurs de etat et etat connexion
     df_raw_data.select('etat', 'etat_connexion').distinct().show()
     ## add df reformed_data et filtre sur le df raw_data
     df_reformed_data = df_raw_data.filter(col("etat") == "RÉFORMÉ")
-    df_raw_data_filtre = df_raw_data.filter(col("etat") != "RÉFORMÉ")
+    df_data_filtre = df_raw_data.filter(col("etat") != "RÉFORMÉ")
     print(f"Nombre de stations réformée: {df_reformed_data.count()}")
     df_reformed_data.show(2)
-    df_raw_data_filtre.select('etat', 'etat_connexion').distinct().show()
+    df_data_filtre.select('etat', 'etat_connexion').distinct().show()
     ## Export reformed vers json
     df_reformed_data.write.mode("overwrite").json(REFORMED_STATIONS_PATH)
-    ## Check valeur distinctes (id, nom, x, y)
-    list_col = ['etat_connexion', 'id', 'nom', 'x', 'y']
+    ## Check doublons (id, nom)
+    list_col = ['id', 'nom']
     for i in list_col:
-        etat_duplicates = df_raw_data_filtre.groupBy(i).count().filter(F.col('count') > 1)
-        print(f"doublons {i} : {etat_duplicates.count()}")
-        # etat_duplicates.show()
-    # extract_doublon = df_raw_data_filtre.filter(col("y") == "50.61843")
-    # extract_doublon.show()
-    test_df = df_raw_data.filter(col("nom") == "PORTE DES POSTES")
-    test_df.show()
+        check_duplicates = df_data_filtre.groupBy(i).count().filter(F.col('count') > 1)
+        print(f"doublons {i} : {check_duplicates.count()}")
+        check_duplicates.show()
 
 if __name__ == "__main__":
     spark = init_or_load_spark()
