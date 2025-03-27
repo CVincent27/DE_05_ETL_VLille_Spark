5fea22975f3db6fc97657b114399cbc7ff3c213c
VincentC27 <18120867+CVincent27@users.noreply.github.com>
2025-03-27 11:51:23 +0100

    add test transform data script

--- src/transform.py
+++ src/transform.py
@@ -11,6 +11,18 @@ def load_raw_data(spark):
     df_raw_data.printSchema()
     return df_raw_data
 
+def transform_data(df_raw_data):
+    if df_raw_data is None:
+        print("Dataframe introuvable")
+        return None
+
+    # 1. Formatage de la data
+    df_format_date = df_raw_data.withColumn('date', F.date_format(F.to_timestamp(df_raw_data['date'], 'yyyy-MM-dd\'T\'HH:mm:ss.SSSXXX'), 'yyyy-MM-dd HH:mm'))
+    df_format_date = df_format_date.orderBy(F.desc('date'))
+    df_format_date.show(1)
+
 if __name__ == "__main__":
     spark = init_or_load_spark()
     df_raw_data = load_raw_data(spark)
+    if df_raw_data:
+        transform_data(df_raw_data)
