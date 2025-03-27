492e4054e4adb6b0c812e4dfafaf3b8860b3a3eb
VincentC27 <18120867+CVincent27@users.noreply.github.com>
2025-03-27 11:57:39 +0100

    test add colonnes

--- src/transform.py
+++ src/transform.py
@@ -1,6 +1,7 @@
 from extract import init_or_load_spark
 from config import RAW_DATA_PATH, os
 from pyspark.sql import functions as F
+from pyspark.sql.functions import col, round
 
 def load_raw_data(spark):
     if not os.path.exists(RAW_DATA_PATH):
@@ -19,7 +20,11 @@ def transform_data(df_raw_data):
     # 1. Formatage de la data
     df_format_date = df_raw_data.withColumn('date', F.date_format(F.to_timestamp(df_raw_data['date'], 'yyyy-MM-dd\'T\'HH:mm:ss.SSSXXX'), 'yyyy-MM-dd HH:mm'))
     df_format_date = df_format_date.orderBy(F.desc('date'))
-    df_format_date.show(1)
+    # df_format_date.show(5)
+
+    # 2. Add nb_places_totales et % full
+    df_add_col = df_format_date.withColumn('nb_places_totales', df_format_date['nb_places_dispo'] + df_format_date['nb_velos_dispo'])
+    df_add_col.show(5)
 
 if __name__ == "__main__":
     spark = init_or_load_spark()
